e7ea6183eef8bb8c361966be30f0c76564dbd431
Erick Oberstar <oberstare@intellivention.com>
2014-07-25 10:00:26 -0500

    Added Caleb's version of the system model & modifications to ControlDesign.m to configure parameters and call his models

--- Hardware Documentation/Matlab Calculations/ControlDesign.m
+++ Hardware Documentation/Matlab Calculations/ControlDesign.m
@@ -1,12 +1,70 @@
 %% Control System Modeling for the compliant contact roller control system
 % Written by Erick Oberstar 5/15/2014
+% Version 2 - Caleb Secrest 7/22/2014
 
 clc; clear all; close all;
 %%*************************************************************************
 %% Physical System Definition
 
-% Analog Input Gain
-K_ADC_9381 = (4095-0)/(5-0) % Counts/Volt
+% Compute Gain for the PWM
+PWM_res = 2000; % Number of counts for pwm
+V_rail = 24;
+K_PWM = PWM_res/V_rail; % Count/Volts
+
+% Firgelli Actuator Force Model (Input Voltage - Output Force)
+K_L12_Force = 0.00229181;          % A/N (Slope on Force vs Current Response)
+% *********  Change to units of A and check saturation block again
+L12_Offset_I = 0.012995; % A (Current Offset on Force vs Current Response)
+Ra_L12 = 60; % Armature Resistance in Ohms of Firgelli
+Mar = 0.075; % Effective Mass of actuator & roller in Kg
+F_max = 60;  % Maximum Allowable Commanded Force [N]
+
+% Compute Back-EMF Constant
+voltage_applied = [1.5 4.5 7.5];
+speed_at_voltage_applied = [0.6 2.16 3.7]; % mm/sec
+i = [0.006 0.0085 0.011];
+Vbackemf = (voltage_applied-(i*Ra_L12));
+K_backemf_array = Vbackemf./speed_at_voltage_applied; % Units ov Volts-s/mm Volts/(mm/s}
+K_backemf = 1000*mean( K_backemf_array ); %[Volt/(m/s)]
+
+% Position Limits
+Home = 0; % Home position (m)
+L_Range = [0 0.050]; % Position Range of actuator (m)
+ContactPoint = 0.023;%0.023; % Contact Position (m)
+
+% Physical Damping (reducing model jitter)
+bp = 100;
+
+% Contact Parameters
+m_surf = 1e3;
+b_surf = 1e5;
+k_surf = 1e8;
+
+%% Position Sensor Stuff
+POS = 0;
+if POS
+    % Compute Gain for Position Sensor
+    R_nom_per_mm = 2750;                                        % Nominal Resistance in Ohms/mm
+    R_tol = 0.3;                                                % Tolerance in % of the position feedback resistance
+    R_lin = 0.01;                                               % Linearity of the Feedback potentiometer
+    R_Range = R_nom_per_mm*[1+R_tol 1 1-R_tol];
+    L_nom = 50;                                                 % length in mm
+    R_MaxPos_Range = R_Range*L_nom;                             %
+    
+    % Actual Resistance doesn't matter - Position is Ratiometric from 0 Ohms to
+    % full scale Ohms over the reference voltage so the pot location is just a
+    % percentage from 0-100% over the length of the actuator across the ADC
+    % reference voltage
+    VPot_Range = [0 5]; % Position Pot Source Voltage Range
+    
+    L_Range_Const = [Home ContactPoint]; % Position Range of actuator is constrained by contact in mm
+    K_L12_Pos_mm_per_volt = (max(L_Range)-min(L_Range))/...
+        (max(VPot_Range)-min(VPot_Range)); % mm/Volt
+    K_L12_Pos_m_per_volt = K_L12_Pos_mm_per_volt/1000; %Convert to m/Volt
+    
+    % Now lump in ADC Gain
+    K_L12_Pos = K_L12_Pos_m_per_volt/K_ADC_9381; % meter/Volt / Counts/Volt = meters/count
+end
 
 % Compute Gain of Force System (DCM460 2/ HPB-20 Transducer
 g = 9.81 % acceleration of gravity on earth (m/s^2)
@@ -16,72 +74,57 @@ VRange = [ 0 3.7] % Units of Volts
 K_DCM460_HPB20 = (max(F)-min(F))/(max(VRange)-min(VRange)) % Units of Newton/Volt
 K_ForceFB = K_DCM460_HPB20 / K_ADC_9381 % Newton/Volt / Counts/Volt = Newton/Count
 
+%% Controller
 
-% Compute Gain for Position Sensor
-R_nom_per_mm = 2750 % Nominal Resistance in Ohms/mm
-R_tol = 0.3 % Tolerance in % of the position feedback resistance
-R_lin = 0.01 % Linearity of the Feedback potentiometer
-R_Range = R_nom_per_mm*[1+R_tol 1 1-R_tol]
-L_nom = 50 % lenght in mm
-R_MaxPos_Range = R_Range*L_nom % 
+% Sampling Frequency
+fs = 1e3;
+Ts = 1/fs;
 
-% Actual Resistance doesn't matter - Position is Ratiometric from 0 Ohms to
-% full scale Ohms over the reference voltage so the pot location is just a
-% percentage from 0-100% over the length of the actuator across the ADC
-% reference voltage
-VPot_Range = [0 5] % Position Pot Source Voltage Range
-L_Range = [0 50] % Position Range of actuator in mm
-Home = 0; % Home position (mm)
-ContactPoint = 23 % Contact Position in (mm)
-L_Range_Const = [Home ContactPoint] % Position Range of actuator is constrained by contact in mm
-K_L12_Pos_mm_per_volt = (max(L_Range)-min(L_Range))/(max(VPot_Range)-min(VPot_Range)) % mm/Volt
-K_L12_Pos_m_per_volt = K_L12_Pos_mm_per_volt/1000 %Convert to m/Volt
+% Virtual Spring Stiffness
+kvc = 20e3;
 
-% Now lump in ADC Gain
-K_L12_Pos = K_L12_Pos_m_per_volt/K_ADC_9381 % meter/Volt / Counts/Volt = meters/count
+% Modulator for Virtual Spring Compression Command
+ymod = 0.75e-3;
 
-% Compute Gain for the PWM
-PWM_res = 2000 % Number of counts for pwm
-V_rail = 24
-K_PWM = PWM_res/V_rail % Count/Volts
+%% Discrete Time Motion Controller Model
+MOTION_CTRL = 0;
+if MOTION_CTRL
+    %Desired Bandwidth
+    f_1 = 150; s_1 = 2*pi*f_1; z_1 = exp(-s_1*Ts);
+    f_2 = f_1/5; s_2 = 2*pi*f_2; z_2 = exp(-s_2*Ts);
+    f_3 = f_2/5; s_3 = 2*pi*f_3; z_3 = exp(-s_3*Ts);
 
-% Firgelli Actuator Force Model (Input Voltage - Output Force)
-K_L12_Force = 2.29181 % mA/N (Slope on Force vs Current Response)
-% *********  Change to units of A and check saturation block again
-L12_Offset_I = 12.995 % mA (Current Offset on Force vs Current Response)
-Ra_L12 = 60 % Armature Resistance in Ohms of Firgelli
-Mar = 0.075 % Effective Mass of actuator & roller in Kg
+    % Calculate Gains
+    Jphat = Mar;
+    ba = (-(z_1*z_2*z_3)*Jphat + Jphat)/Ts;
+    ksa = (-(z_1*z_2 + z_1*z_3 + z_2*z_3)*Jphat + 3*Jphat - 2*Ts*ba)/(Ts^2);
+    kisa = (-(z_1 + z_2 + z_3)*Jphat + 3*Jphat - Ts*ba - (Ts^2)*ksa)/(Ts^3);
+end
 
+%% Command State Filter
+SF = 1;
+if SF
+    %Desired Poles
+    f_1 = 0.1; s_1 = 2*pi*f_1; z_1 = exp(-s_1*Ts);
+    f_2 = 0.1; s_2 = 2*pi*f_2; z_2 = exp(-s_2*Ts);
 
-%% backout backemf constant for the linear motor
-voltage_applied = [1.5 4.5 7.5]
-speed_at_voltage_applied = [0.6 2.16 3.7] % mm/sec
-i = [0.006 0.0085 0.011]
-Vbackemf = (voltage_applied-(i*Ra_L12))
-K_backemf_array = Vbackemf./speed_at_voltage_applied % Units ov Volts-s/mm Volts/(mm/s}
-K_backemf = min( K_backemf_array )
+    k_2 = (2-(z_1+z_2))/Ts;
+    k_1 = (z_1*z_2-1+k_2*Ts)/(k_2*Ts^2);
 
-%% Firgelli Actuator Position Model
+    % Velocity Limit
+    v_lim = 0.003;
+    % Acceleration Limit
+    a_lim = 0.75*F_max/Mar;
+end
 
+%% Simulate
 %Simulation Model Configuration Parameters
 StartTime = 0;
 StopTime = 15;
 MaxStepSize = 0.1;
 
-Kspring = 55; % Virtual Spring Constant (N/mm)
-Kdamper = 5*1000; % Virtual damper N-s/mm (Force/Velocity) *1000 converts /m to /mm
-
-% Kspring = 5.110718747826613e+06
-% Kdamp = 3.497032275548465e+09
-PulsePeakAmplitude = 5
-
-% Open Simulink
-simulink
-% Open & sim the Model
-R2RSystemModel
-sim('R2RSystemModel');
-% R2RSystemModel_PosControl
-% sim('R2RSystemModel_PosControl');
+set_param('CalebsModel_v3','AlgebraicLoopSolver','LineSearch')
+sim('CalebsModel_v3')
 
 % find all scope blocks as MATLAB figures & Set to autoscale:
 set(0, 'showhiddenhandles', 'on')
@@ -89,41 +132,3 @@ scope = findobj(0, 'Tag', 'SIMULINK_SIMSCOPE_FIGURE');
 for i=1:length(scope)
   % this is the callback of the "autoscale" button:
   simscope('ScopeBar', 'ActionIcon', 'Find', scope(i))
-end
-set(0, 'showhiddenhandles', 'off')
-
-% %%*************************************************************************
-% %% Design a low pass state filter for the virtual compliance y* command signal
-% Ts = 40000/40e6 % from crio-FPGA-control\Globals\Global - Configuration Options.vi
-% 
-% fc = 500
-% 
-% filt = tf(1,[1/(2*pi*fc) 1])
-% 
-% figure; 
-% bode(filt)
-% h = gcr; % Get the handle for the "plot object root"
-% % which is a structure containing the plot properties
-% % Set frequency axis to Hz
-% h.AxesGrid.Xunits = 'Hz';
-% % Set mag axis to abs & phase axis to deg
-% h.AxesGrid.Yunits = {'abs','deg'};
-% % Turn on the grid
-% grid on; % or h.AxesGrid.Grid = "on"
-% 
-% filt_d = c2d(filt,Ts,'zoh');
-% filt_d.Variable = 'z^-1'
-% filt_d
-% 
-% figure; 
-% bode(filt_d)
-% h = gcr; % Get the handle for the "plot object root"
-% % which is a structure containing the plot properties
-% % Set frequency axis to Hz
-% h.AxesGrid.Xunits = 'Hz';
-% % Set mag axis to abs & phase axis to deg
-% h.AxesGrid.Yunits = {'abs','deg'};
-% % Turn on the grid
-% grid on; % or h.AxesGrid.Grid = "on"
-% 
-% 
